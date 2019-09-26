8b52867a4cd9ee75f1ec2edbf7727dd44b738b9b
Jacky Feng <jackyfeng530@yahoo.com>
2019-09-26 18:50:33 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -92,7 +92,9 @@ class Student
     ORDER BY students.id
     LIMIT 1
     SQL
-    DB[:conn].execute(sql)
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end 
   end 
   
 end
