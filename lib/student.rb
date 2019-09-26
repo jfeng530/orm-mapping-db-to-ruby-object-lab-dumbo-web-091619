5022045c468341544dc29a1ee4b8bd3fdc68ab7a
Jacky Feng <jackyfeng530@yahoo.com>
2019-09-26 18:39:04 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -52,4 +52,12 @@ class Student
     sql = "DROP TABLE IF EXISTS students"
     DB[:conn].execute(sql)
   end
+  
+  def self.all_students_in_grade_9
+    sql = <<-SQL
+    SELECT * FROM students
+    WHERE grade = "
+   SQL
+   DB[:conn].execute(sql)
+  end 
 end
