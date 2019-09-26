e9ea0dbc7d4697e4149b3d68c304530d75172c8a
Jacky Feng <jackyfeng530@yahoo.com>
2019-09-26 18:49:32 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -85,4 +85,14 @@ class Student
    end
   end 
   
+  def self.first_student_in_grade_10 
+    sql = <<-SQL
+    SELECT * FROM students
+    WHERE grade = 10
+    ORDER BY students.id
+    LIMIT 1
+    SQL
+    DB[:conn].execute(sql)
+  end 
+  
 end
